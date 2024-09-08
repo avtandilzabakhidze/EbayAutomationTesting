15a837e5196343e409895abf96826110c4f6df02
avtandilzabakhidze <zabakhidze01@gmail.com>
2024-09-08 19:22:44 +0400

    Add CartPage

---
+++ src/main/java/com/ebay/pages/CartPage.java
@@ -0,0 +1,74 @@
+package com.ebay.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+import java.util.Set;
+
+public class CartPage extends BasePage {
+    private By addedProductNumber = By.id("gh-cart-n");
+    private By cart = By.id("gh-minicart-hover");
+    private By searchInput = By.id("gh-ac");
+    private By submitButton = By.id("gh-btn");
+    private By findProducts = By.xpath("//li[contains(@id,'item')]");
+    private By productTitle = By.xpath("//li[contains(@id,'item')]//span[@role='heading']");
+    private By addToCart = By.id("atcBtn_btn_1");
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public int addOneProductToCart(String productName, int number) {
+        searchElement(productName);
+
+        waitForClickable(findProducts);
+        List<WebElement> products = findElements(findProducts);
+        WebElement product = products.get(number);
+        WebElement titleElement = product.findElement(productTitle);
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", titleElement);
+        wait.until(ExpectedConditions.elementToBeClickable(titleElement)).click();
+
+        switchToNewWindow();
+        wait.until(ExpectedConditions.elementToBeClickable(addToCart)).click();
+        switchToOriginalWindow();
+
+        return getCartCount();
+    }
+
+    private void searchElement(String text) {
+        find(searchInput).clear();
+        find(searchInput).sendKeys(text);
+        find(submitButton).click();
+    }
+
+    private void switchToNewWindow() {
+        String originalWindow = driver.getWindowHandle();
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String windowHandle : allWindows) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+    }
+
+    private void switchToOriginalWindow() {
+        String originalWindow = driver.getWindowHandle();
+        driver.switchTo().window(originalWindow);
+    }
+
+    private int getCartCount() {
+        String addedNumber = getText(addedProductNumber);
+        try {
+            return Integer.parseInt(addedNumber);
+        } catch (NumberFormatException e) {
+            System.err.println("Cart count could not be parsed: " + addedNumber);
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/CardPageTest.java
@@ -0,0 +1,35 @@
+import com.ebay.pages.CartPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CardPageTest {
+    private WebDriver driver;
+    private CartPage cartPage;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        cartPage = new CartPage(driver);
+        driver.manage().window().maximize();
+        driver.get("https://www.ebay.com");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testAddedOneProduct() {
+        int currentAddedProducts = cartPage.addOneProductToCart("book", 2);
+        int expectedNumber = 1;
+
+        Assert.assertEquals(currentAddedProducts, expectedNumber, "\n Product did not add \n");
+    }
+}
